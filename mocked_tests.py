d13d79005da02a2bfa285ef264bd0098063b7bfd
naimam <naimamohamed19@hotmail.com>
2021-10-26 22:14:23 +0000

    adding server test files

---
+++ mocked_tests.py
@@ -0,0 +1,9 @@
+import unittest
+import unittest.mock as mock
+from unittest.mock import patch
+
+from app import Artist
+
+# Each test should extend unittest.TestCase and use the mocking framework in Lecture 13
+# Each test should test logic of 1 unique function in your app. None should make any real DB/Socket calls, but instead mock them
+# Each test should have 3+ test params (inputs) and 2+ assert statements, and pass when ran
---
+++ unmocked_tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+# 2 tests
+# Each should extend unittest.TestCase
+# Each test should test logic of 1 unique function in your app, not start up a local Flask server or test socket.io functionality
+# Each test should have 3+ test params (inputs) and 2+ assert statements, and pass when ran
